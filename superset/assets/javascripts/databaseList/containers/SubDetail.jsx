5c681757636083d8e24623068ff8846c714377d1
rockGithub <rock20110414@gmail.com>
2017-06-12 04:07:57 -0700

    details dom changes

--- superset/assets/javascripts/databaseList/containers/SubDetail.jsx
+++ superset/assets/javascripts/databaseList/containers/SubDetail.jsx
@@ -11,34 +11,104 @@ export default function SubDetail (){
                 </label>
                 <label className="data-detail-item">
                     <span>数据集类型：</span>
-                    <p>inceptor</p>
+                    <dl>
+                        <dt><dd className="radio-glugin-active radio-glugin"></dd></dt>
+                        <dd className="active">inceptor</dd>
+                    </dl>
+                    <dl>
+                        <dt><dd className="radio-glugin"></dd></dt>
+                        <dd>HDFS</dd>
+                    </dl>
+                    <dl>
+                        <dt><dd className="radio-glugin"></dd></dt>
+                        <dd>上传文件</dd>
+                    </dl>
+                </label>
+
+                <label className="data-detail-item">
+                    <span></span>
+                    <div className="file-show">
+                        <div className="file-fold">
+                            <i className="icon"></i>
+                            <span>DEV</span>
+                        </div>
+                        <div className="file-fold-active">
+                            <i className="icon"></i>
+                            <span>AD</span>
+                            <div className="file-fold">
+                                <i className="icon"></i>
+                                <span>DEV</span>
+                            </div>
+                            <div className="file-fold">
+                                <i className="icon"></i>
+                                <span className="active">DEV</span>
+                            </div>
+                            <div className="file-fold">
+                                <i className="icon"></i>
+                                <span>DEV</span>
+                            </div>
+                        </div>
+                        <div className="file-fold">
+                            <i className="icon"></i>
+                            <span>DEV</span>
+                        </div>
+                        <div className="file-fold">
+                            <i className="icon"></i>
+                            <span>DEV</span>
+                        </div>
+                        <div className="file-fold">
+                            <i className="icon"></i>
+                            <span>DEV</span>
+                        </div>
+                    </div>
                 </label>
                 <label className="data-detail-item">
-                    <span>连接：</span>
-                    <p>inceptor</p>
+                    <span>路径：</span>
+                    <input type="text" value="" />
                 </label>
                 <label className="data-detail-item">
-                    <span>数据库：</span>
-                    <p>inceptor</p>
+                    <span></span>
+                    <button className="uploading-btn">上传文件</button>
                 </label>
                 <label className="data-detail-item">
-                    <span>表：</span>
-                    <p>inceptor</p>
+                    <span></span>
+                    <div className="file-uploading">
+                        <i className="icon"></i>
+                        <span>package.json</span>
+                        <div className="progress"></div>
+                    </div>
+
+                </label>
+                <label className="data-detail-item">
+                    <span></span>
+                    <div className="file-uploaded">
+                        <i className="icon"></i>
+                        <span>package.json</span>
+                        <div className="finish"></div>
+                    </div>
+                </label>
+                <label className="data-detail-item">
+                    <span></span>
+                    <div className="connect-success">
+                        <span>连接成功</span>
+                        <button>配置></button>
+                    </div>
                 </label>
                 <label className="data-detail-item">
                     <span>描述：</span>
                     <textarea name="" id="" cols="30" rows="10"></textarea>
                 </label>
                 <div className="data-detail-wrap-item">
-                    <label className="data-detail-item">
+                    <label className="data-detail-item data-detail-item-time">
                         <span>主列时间：</span>
                         <input type="text"/>
                     </label>
-                    <label className="data-detail-item">
+                    <label className="data-detail-item data-detail-item-time">
                         <span>缓存时间：</span>
                         <input type="text" />
                     </label>
                 </div>
+
             </div>
             <label className="sub-btn">
                 <input type="button" defaultValue="保存" />
