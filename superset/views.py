30c12bd0a00b945ca04855e2d629d4a4cc215259
jiajie <jiajie.zhang@transwarp.io>
2017-06-12 13:34:53 +0800

    WARP-14912 remove get_edited_slices_api() get_edited_dashboards_api() get_user_actions_api() in Home

--- superset/views.py
+++ superset/views.py
@@ -3609,32 +3609,6 @@ class Home(BaseSupersetView):
                             status=status_,
                             mimetype='application/json')
 
-    def get_edited_slices_api(self, **kwargs):
-        kwargs = self.get_request_args(kwargs)
-        kwargs['user_id'] = self.get_user_id()
-        count, data = self.get_edited_slices(**kwargs)
-        response = {}
-        response['data'] = data
-        response['count'] = count
-        response['page'] = kwargs.get('page')
-        response['page_size'] = kwargs.get('page_size')
-        response['order_column'] = kwargs.get('order_column')
-        response['order_direction'] = kwargs.get('order_direction')
-        return json.dumps(response)
-
-    def get_edited_dashboards_api(self, **kwargs):
-        kwargs = self.get_request_args(kwargs)
-        kwargs['user_id'] = self.get_user_id()
-        count, data = self.get_edited_dashboards(**kwargs)
-        response = {}
-        response['data'] = data
-        response['count'] = count
-        response['page'] = kwargs.get('page')
-        response['page_size'] = kwargs.get('page_size')
-        response['order_column'] = kwargs.get('order_column')
-        response['order_direction'] = kwargs.get('order_direction')
-        return json.dumps(response)
-
     def get_user_actions(self, **kwargs):
         """The actions of user"""
         user_id = kwargs.get('user_id')
@@ -3699,20 +3673,6 @@ class Home(BaseSupersetView):
                     }
             rows.append(line)
         return count, rows
-
-    def get_user_actions_api(self, **kwargs):
-        kwargs = self.get_request_args(kwargs)
-        kwargs['user_id'] = self.get_user_id()
-        kwargs['types'] = kwargs.get('types', self.default_types.get('actions'))
-        count, data = self.get_user_actions(**kwargs)
-        response = {}
-        response['data'] = data
-        response['count'] = count
-        response['page'] = kwargs.get('page')
-        response['page_size'] = kwargs.get('page_size')
-        response['order_column'] = kwargs.get('order_column')
-        response['order_direction'] = kwargs.get('order_direction')
-        return json.dumps(response)
 
     @expose('/actions/')
     def get_user_actions_by_url(self):
