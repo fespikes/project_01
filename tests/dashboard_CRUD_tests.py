66b422668d405cffb57f940815ce97f3fdd431ad
lc <lc_yizhiyi@163.com>
2017-06-21 20:46:58 +0800

    WARP-15022 modify dashboard crud file

--- tests/dashboard_CRUD_tests.py
+++ tests/dashboard_CRUD_tests.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import unittest
+import logging
 
 from sqlalchemy import and_, or_
 from flask_appbuilder.security.sqla.models import User
@@ -12,29 +13,14 @@ from superset import db
 from superset.models import Dashboard
 from superset.views import DashboardModelView
 from tests.base_tests import SupersetTestCase
-
-
-class PageMixin(object):
-    order_column = 'changed_on'
-    order_direction = 'desc'
-    page = 0
-    page_size = 10
-    filter = None
-    only_favorite = False
-    kwargs = {'order_column': order_column,
-              'order_direction': order_direction,
-              'page': page,
-              'page_size': page_size,
-              'filter': filter,
-              'only_favorite': only_favorite}
-
+from tests.base_tests import PageMixin
 
 class DashboardCRUDTests(SupersetTestCase, PageMixin):
     requires_examples = True
 
     def __init__(self, *args, **kwargs):
         super(DashboardCRUDTests, self).__init__(*args, **kwargs)
-        self.dash_view = DashboardModelView()
+        self.view = DashboardModelView()
 
     def setUp(self):
         pass
@@ -43,17 +29,12 @@ class DashboardCRUDTests(SupersetTestCase, PageMixin):
         pass
 
     def test_listdata(self):
-        kwargs = self.kwargs
-        kwargs['user_id'] = self.user.id
-        real_value = self.dash_view.get_object_list_data(**kwargs)
+        self.check_base_param()
 
-        assert self.page == real_value.get('page')
-        assert self.page_size == real_value.get('page_size')
-        assert self.order_column == real_value.get('order_column')
-        assert self.order_direction == real_value.get('order_direction')
-
-        dash_list = real_value.get('data')
-        one_dash = dash_list[0]
+        dash_list = self.real_value.get('data')
+        one_dash = None
+        if dash_list:
+            one_dash = dash_list[0]
         for dash_dict in dash_list:
             assert isinstance(dash_dict.get('id'), int)
             assert isinstance(dash_dict.get('online'), bool)
@@ -70,28 +51,29 @@ class DashboardCRUDTests(SupersetTestCase, PageMixin):
 
     def test_show(self):
         one_dash = db.session.query(Dashboard).first()
-        real_value = self.dash_view.get_show_attributes(one_dash, self.user.id)
-        assert one_dash.id == real_value.get('id')
-        assert one_dash.dashboard_title == real_value.get('dashboard_title')
-        assert one_dash.description == real_value.get('description')
-        assert one_dash.table_names == real_value.get('table_names')
-        assert len(one_dash.slices) == len(real_value.get('slices'))
+        self.real_value = self.view.get_show_attributes(one_dash, self.user.id)
+        assert one_dash.id == self.real_value.get('id')
+        assert one_dash.dashboard_title == self.real_value.get('dashboard_title')
+        assert one_dash.description == self.real_value.get('description')
+        assert one_dash.table_names == self.real_value.get('table_names')
+        assert len(one_dash.slices) == len(self.real_value.get('slices'))
 
     def test_add_edit_delete(self):
         new_dash_id = None
 
         # add
-        new_slices = self.dash_view.get_available_slices(self.user.id)[:2]
+        new_slices = self.view.get_available_slices(self.user.id)[:2]
         new_dash = {'dashboard_title': 'new_dashboard',
                     'description': 'new dashboard',
-                    'slices': self.dash_view.slices_to_dict(new_slices)}
-        obj = self.dash_view.populate_object(None, self.user.id, new_dash)
-        self.dash_view.datamodel.add(obj)
+                    'slices': self.view.slices_to_dict(new_slices)}
+        obj = self.view.populate_object(None, self.user.id, new_dash)
+        self.view.datamodel.add(obj)
 
         added_dash = db.session.query(Dashboard)\
             .filter_by(dashboard_title=new_dash.get('dashboard_title'))\
             .first()
         new_dash_id = added_dash.id
+
         assert added_dash.dashboard_title == new_dash.get('dashboard_title')
         assert added_dash.description == new_dash.get('description')
         # when add/edit in app, 'created_by_fk' and 'changed_by_fk'
@@ -101,12 +83,12 @@ class DashboardCRUDTests(SupersetTestCase, PageMixin):
             assert slc.slice_name in new_slices_name
 
         # edit
-        new_slices = self.dash_view.get_available_slices(self.user.id)[0:2]
+        new_slices = self.view.get_available_slices(self.user.id)[0:2]
         new_dash = {'dashboard_title': 'edited_dashboard',
                     'description': 'edit dashboard',
-                    'slices': self.dash_view.slices_to_dict(new_slices)}
-        obj = self.dash_view.populate_object(new_dash_id, self.user.id, new_dash)
-        self.dash_view.datamodel.edit(obj)
+                    'slices': self.view.slices_to_dict(new_slices)}
+        obj = self.view.populate_object(new_dash_id, self.user.id, new_dash)
+        self.view.datamodel.edit(obj)
 
         edited_dash = db.session.query(Dashboard) \
             .filter_by(id=new_dash_id)\
@@ -118,8 +100,8 @@ class DashboardCRUDTests(SupersetTestCase, PageMixin):
             assert slc.slice_name in new_slices_name
 
         # delete
-        obj = self.dash_view.get_object(new_dash_id)
-        self.dash_view.datamodel.delete(obj)
+        obj = self.view.get_object(new_dash_id)
+        self.view.datamodel.delete(obj)
         target_dash = db.session.query(Dashboard) \
             .filter_by(id=new_dash_id).first()
         assert target_dash is None
