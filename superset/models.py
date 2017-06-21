f95a9f20953b9fca6168648ed56433b8bb902f6d
jiajie <jiajie.zhang@transwarp.io>
2017-06-21 15:22:12 +0800

    WARP-14006 correct the property: edit_url

--- superset/models.py
+++ superset/models.py
@@ -313,7 +313,7 @@ class Slice(Model, AuditMixinNullable, ImportMixin):
 
     @property
     def edit_url(self):
-        return "/slicemodelview/edit/{}".format(self.id)
+        return "/slice/edit/{}".format(self.id)
 
     @property
     def slice_link(self):
