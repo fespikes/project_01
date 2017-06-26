3c0e0c1a503f224c820b5e951c72141e045bdaa3
jiajie <jiajie.zhang@transwarp.io>
2017-06-26 15:32:25 +0800

    WARP-15195 add 'description' for HDFSConnection

--- superset/models.py
+++ superset/models.py
@@ -2066,6 +2066,7 @@ class HDFSConnection(Model, AuditMixinNullable):
 
     id = Column(Integer, primary_key=True)
     connection_name = Column(String(256), nullable=False, unique=True)
+    description = Column(Text)
     database_id = Column(Integer, ForeignKey('dbs.id'))
     webhdfs_url = Column(String(256), nullable=False)
     fs_defaultfs = Column(String(256), nullable=False)
