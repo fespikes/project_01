a3537e983e4845c8b06354931360c623cd52e56d
jiajie <jiajie.zhang@transwarp.io>
2017-06-28 20:07:12 +0800

    WARP-15707 add list hdfs-conenction api

--- superset/views.py
+++ superset/views.py
@@ -942,8 +942,50 @@ class HDFSConnectionModelView(SupersetModelView):
     model = models.HDFSConnection
     datamodel = SQLAInterface(models.Database)
     route_base = '/hdfsconnection'
+    _list_columns = ['id', 'connection_name']
     show_columns = []
     edit_columns = []
+
+    def get_object_list_data(self, **kwargs):
+        """Return the hdfs connections.
+        There won't be a lot of hdfs conenctions, so just use 'page_size'
+        """
+        order_column = kwargs.get('order_column')
+        order_direction = kwargs.get('order_direction')
+        page = kwargs.get('page')
+        page_size = kwargs.get('page_size')
+        filter = kwargs.get('filter')
+        user_id = kwargs.get('user_id')
+
+        query = db.session.query(HDFSConnection, User) \
+            .filter(HDFSConnection.created_by_fk == User.id,
+                    HDFSConnection.created_by_fk == user_id)
+        count = query.count()
+
+        query = query.order_by(HDFSConnection.connection_name.desc())
+        query = query.limit(page_size)
+
+        rs = query.all()
+        data = []
+        for obj, user in rs:
+            line = {}
+            for col in self._list_columns:
+                if col in self.str_columns:
+                    line[col] = str(getattr(obj, col, None))
+                else:
+                    line[col] = getattr(obj, col, None)
+            line['created_by_user'] = obj.created_by.username \
+                if obj.created_by else None
+            data.append(line)
+
+        response = {}
+        response['count'] = count
+        response['order_column'] = order_column
+        response['order_direction'] = 'desc' if order_direction == 'desc' else 'asc'
+        response['page'] = page
+        response['page_size'] = page_size
+        response['data'] = data
+        return response
 
     def add(self):
         pass
