305a1a9670cb425b1bf1d04634b490462e24ec4a
jiajie <jiajie.zhang@transwarp.io>
2017-07-21 13:44:45 +0800

    WARP-16050 add the structure of hdfs-api to call hdfs-micro-service-client-api

--- superset/views.py
+++ superset/views.py
@@ -15,37 +15,33 @@ import re
 from distutils.util import strtobool
 import requests
 import functools
-import sqlalchemy as sqla
+import urllib
 
 from flask import (g, request, redirect, flash, Response, render_template)
-from flask import session as flask_session
 from flask_appbuilder import ModelView, BaseView, expose
 from flask_appbuilder.models.sqla.interface import SQLAInterface
 from flask_appbuilder.security.decorators import has_access
 from flask_appbuilder.security.sqla import models as ab_models
 from flask_appbuilder.security.views import AuthDBView
-
+from flask_appbuilder.security.sqla.models import User
 from flask_babel import gettext as __
-from flask_babel import lazy_gettext as _
 
+import sqlalchemy as sqla
 from sqlalchemy import create_engine, select, literal, cast
+from sqlalchemy import func, and_, or_
 from wtforms.validators import ValidationError
 
 from superset import (
     app, appbuilder, cache, db, models, sm, sql_lab, sql_parse,
-    results_backend, security, viz, utils
-)
+    results_backend, viz, utils)
 from superset.source_registry import SourceRegistry
 from superset.sql_parse import SupersetQuery
-from superset.utils import (get_database_access_error_msg,
-                            get_datasource_access_error_msg,
-                            get_datasource_exist_error_msg,
-                            json_error_response)
 
+from superset.utils import (
+    get_database_access_error_msg, get_datasource_access_error_msg,
+    json_error_response, SupersetException)
 from superset.models import Database, Dataset, Slice, Dashboard, \
     FavStar, Log, DailyNumber, HDFSTable, HDFSConnection, str_to_model
-from sqlalchemy import func, and_, or_
-from flask_appbuilder.security.sqla.models import User
 from superset.message import *
 
 
@@ -3452,44 +3448,120 @@ class Home(BaseSupersetView):
             mimetype="application/json")
 
 
-class HDFSBrowser(BaseSupersetView):
+class HDFSBrowser(BaseView):
     route_base = '/hdfs'
+    header = {"Content-type": "application/json", "Accept":  "application/json"}
+    timeout = 10  # seconds
+
+    def __init__(self):
+        super(HDFSBrowser, self).__init__()
+        self.session = requests.Session()
 
     @catch_exception
-    @expose('/list')
-    def list(self):
+    @expose('/render_html/')
+    def render_html(self):
         return self.render_template('superset/hdfsList.html')
 
     @catch_exception
     @expose('/login/', methods=['GET'])
-    def login_micro_service(self):
+    def login(self):
+        url = '{}/login'.format(self.url_root)
         hdfs_conn_id = request.args.get('hdfs_conn_id')
-        try:
-            if hdfs_conn_id:
-                conn = db.session.query(HDFSConnection)\
-                    .filter_by(id=hdfs_conn_id).first()
-            else:
-                conn = db.session.query(HDFSConnection)\
-                    .order_by(HDFSConnection.id).first()
-            if not conn:
-                logging.error("No hdfs connections, login hdfs-micro-service failed.")
-            server = app.config.get('HDFS_MICROSERVICES_SERVER')
-            cookie = self.login_action(requests.Session(), server, g.user.username,
-                             AuthDBView.mock_user.get(g.user.username), conn.httpfs)
-            logging.info('Login hdfs-micro-service succeed.')
-            # return json.dumps({'cookie': cookie})
-            from flask import make_response
-            response = make_response('OK')
-            response.set_cookie('session', cookie.get('session'), domain='172.16.130.60')
-            return response
-        except Exception as e:
-            logging.error(e)
-            flash('Login hdfs-micro-service failed.')
+        if hdfs_conn_id:
+            conn = db.session.query(HDFSConnection) \
+                .filter_by(id=hdfs_conn_id).first()
+        else:
+            conn = db.session.query(HDFSConnection) \
+                .order_by(HDFSConnection.id).first()
+        data = {"username": g.user.username,
+                "password": AuthDBView.mock_user.get(g.user.username),
+                "httpfshost": conn.httpfs}
+        resp = self.session.post(url, data=data)
+        return self.build_response(resp)
+
+    @catch_exception
+    @expose('/logout/', methods=['GET'])
+    def logout(self):
+        url = '{}/logout'.format(self.url_root)
+        resp = self.session.post(url)
+        return self.build_response(resp)
+
+    @catch_exception
+    @expose('/list/', methods=['GET'])
+    def list(self):
+        url = self.build_url(self.url_prefix, 'list', **request.args.to_dict())
+        return json.dumps(url)
+
+    @catch_exception
+    @expose('/download/', methods=['GET'])
+    def download(self):
+        url = self.build_url(self.url_prefix, 'download', **request.args.to_dict())
+        return json.dumps(url)
+
+    @catch_exception
+    @expose('/upload/', methods=['POST'])
+    def upload(self):
+        url = self.build_url(self.url_prefix, 'upload', **request.args.to_dict())
+        return json.dumps(url)
+
+    @catch_exception
+    @expose('/remove/', methods=['GET'])
+    def remove(self):
+        url = self.build_url(self.url_prefix, 'remove', **request.args.to_dict())
+        return json.dumps(url)
+
+    @catch_exception
+    @expose('/move/', methods=['GET'])
+    def move(self):
+        url = self.build_url(self.url_prefix, 'move', **request.args.to_dict())
+        return json.dumps(url)
+
+    @catch_exception
+    @expose('/mkdir/', methods=['GET'])
+    def mkdir(self):
+        url = self.build_url(self.url_prefix, 'mkdir', **request.args.to_dict())
+        return json.dumps(url)
+
+    @catch_exception
+    @expose('/rmdir/', methods=['GET'])
+    def rmdir(self):
+        url = self.build_url(self.url_prefix, 'rmdir', **request.args.to_dict())
+        return json.dumps(url)
+
+    @catch_exception
+    @expose('/preview/', methods=['GET'])
+    def preview(self):
+        url = self.build_url(self.url_prefix, 'preview', **request.args.to_dict())
+        return json.dumps(url)
+
+    @catch_exception
+    @expose('/chmod/', methods=['GET'])
+    def chmod(self):
+        url = self.build_url(self.url_prefix, 'chmod', **request.args.to_dict())
+        return json.dumps(url)
+
+    @property
+    def url_root(self):
+        server = app.config.get('HDFS_MICROSERVICES_SERVER')
+        if not server:
+            raise SupersetException('No HDFS_MICROSERVICES_SERVER in config.')
+        return 'http://{}'.format(server)
+
+    @property
+    def url_prefix(self):
+        return '{}/filebrowser'.format(self.url_root)
+
+    def build_url(self, url_prefix, action, **args):
+        args['action'] = action
+        return '{}?{}'.format(url_prefix, urllib.parse.urlencode(args))
+
+    def build_response(self, resp):
+        if resp.status_code != requests.codes.ok:
+            resp.raise_for_status()
+        return Response()
 
     @classmethod
     def login_action(cls, req, server, username, password, httpfs):
-        server = '172.16.201.240:5000'
-        httpfs = '172.16.130.60'
         data = {"username": username,
                 "password": password,
                 "httpfshost": httpfs}
