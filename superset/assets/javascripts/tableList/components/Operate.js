77e216e1d1d239f4c8c2c92a5550e47339034f1d
haitao <haitao.lv@transwarp.io>
2017-08-03 15:25:08 +0800

    fixed bug: remove upload dataset type

--- superset/assets/javascripts/tableList/components/Operate.js
+++ superset/assets/javascripts/tableList/components/Operate.js
@@ -97,6 +97,7 @@ class SliceOperate extends React.Component {
     render() {
         const Option = Select.Option;
         let datasetTypes = JSON.parse(JSON.stringify(this.state.datasetTypes));
+        datasetTypes.splice(datasetTypes.length-1, 1);
         datasetTypes.splice(0, 0, 'all');
         const filterOptions = datasetTypes.map(dataset => {
             return <Option key={dataset} value={dataset}>{dataset}</Option>
