a82bfc6acc8adec73da0ac093b725114b97f85ea
jiajie <jiajie.zhang@transwarp.io>
2017-08-22 12:10:15 +0800

    WARP-16232 modify the configy COMMMUNITY_EDITION to true

--- superset/config.py
+++ superset/config.py
@@ -39,7 +39,7 @@ CUSTOM_SECURITY_MANAGER = None
 # ---------------------------------------------------------
 
 # The Community Edition will abandon guardian module, and embed user management module
-COMMUNITY_EDITION = False
+COMMUNITY_EDITION = True
 
 # if load examples data when start server
 LOAD_EXAMPLES = True
