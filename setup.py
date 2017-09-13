a518c3c1b2f7ae258be36157529bccce0ae9f9d6
jiajie <jiajie.zhang@transwarp.io>
2017-09-13 15:52:52 +0800

    WARP-17360 add dependence for oracle connection

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
     install_requires=[
         'celery==3.1.23',
         'cryptography==1.5.3',
+        'cx_Oracle==5.3',
         'fileRobot-client==0.0.1',
         'flask-appbuilder==1.8.1',
         'flask-cache==0.13.1',
