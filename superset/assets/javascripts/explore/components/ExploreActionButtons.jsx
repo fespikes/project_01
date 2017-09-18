44d34ebbf4cedbf2cfccd9d473b74b3b6c8aeff2
haitao <haitao.lv@transwarp.io>
2017-09-18 19:12:47 +0800

    delete unused files

--- superset/assets/javascripts/explore/components/ExploreActionButtons.jsx
+++ superset/assets/javascripts/explore/components/ExploreActionButtons.jsx
@@ -4,7 +4,6 @@ import cx from 'classnames';
 import URLShortLinkButton from './URLShortLinkButton';
 import EmbedCodeButton from './EmbedCodeButton';
 import DisplayQueryButton from './DisplayQueryButton';
-import SliceEdit from './SliceEdit';
 import { Tooltip } from 'antd';
 
 const propTypes = {
@@ -17,43 +16,8 @@ export default function ExploreActionButtons({ canDownload, slice, sliceId, quer
   const exportToCSVClasses = cx('btn btn-default btn-sm', {
     'disabled disabledButton': !canDownload,
   });
-  function editSlice() {
-      $.ajax({
-          type: 'GET',
-          url: '/slice/show/' + sliceId,
-          contentType: 'application/json',
-          success(data) {
-              let slice = JSON.parse(data);
-              let editSlicePopup = render(
-                  <SliceEdit
-                      slice={slice}/>,
-                  document.getElementById('popup_root'));
-              if(editSlicePopup) {
-                  editSlicePopup.showDialog();
-              }
-          },
-          error(error) {
-
-          }
-      });
-  }
   return (
     <div className="btn-group results" role="group">
-        {/*
-            <a className="btn btn-default btn-sm" onClick={() => editSlice()}>
-                <i className="fa fa-edit"></i>
-            </a>
-            <URLShortLinkButton slice={slice} />
-            <EmbedCodeButton slice={slice} />
-            <a
-                href={slice.data.json_endpoint}
-                className="btn btn-default btn-sm"
-                title="Export to .json"
-                target="_blank"
-            >
-                <i className="fa fa-file-code-o"></i>.json
-            </a>
-        */}
         <Tooltip title={'导出为.csv格式'} placement="bottom">
             <a
                 href={slice.data.csv_endpoint}
