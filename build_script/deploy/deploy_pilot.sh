07c53a3a48b542844ca9ea33167cd3d5321cf316
jiajie <jiajie.zhang@transwarp.io>
2017-12-01 17:31:08 +0800

    WARP-19454 move build scripts to project

---
+++ build_script/deploy/deploy_pilot.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+function deploy_pilot
+{
+   echo "[DEPLOY_NOTICE]: nothing to deploy :)"
+}
---
+++ build_script/rpm/build_pilot.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+function build_pilot
+{
+   echo "[ERROR]: have no Pilot RPM build scripts now !!!"
+   exit 1
+
+}
