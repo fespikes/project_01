2853d528c8f61b7a09b7e20d28fa940217982842
haitao <haitao.lv@transwarp.io>
2017-12-11 17:15:13 +0800

    pilot:rewrite confirm dialog

--- superset/assets/javascripts/common/components/index.js
+++ superset/assets/javascripts/common/components/index.js
@@ -1,3 +1,7 @@
+/**
+ * Global components in pilot
+ */
 export LoadingModal from './LoadingModal';
 export ConfirmModal from './ConfirmModal';
 export OperationSelect from './OperationSelect';
+export WarningSign from './WarningSign';
