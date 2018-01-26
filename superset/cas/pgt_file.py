56298445e85e1eee1a11db514147508a7ac9c383
jiajie <jiajie.zhang@transwarp.io>
2018-01-26 16:16:17 +0800

    WARP-20843 fix pgt file can't read all lines

--- superset/cas/pgt_file.py
+++ superset/cas/pgt_file.py
@@ -4,40 +4,36 @@ import threading
 
 
 lock = threading.Lock()
-read_rows = 100
-pgt_file = '/tmp/pilot.cas'
 
 
 class PgtFile(object):
 
+    pgt_file = '/tmp/pilot.cas'
+    read_max_bytes = 16 * 1024
+
     @classmethod
     def get_pgt(cls, pgtiou):
-        if not os.path.exists(pgt_file):
-            logging.error('Pgt file: [{}] is not existed'.format(pgt_file))
+        if not os.path.exists(cls.pgt_file):
+            logging.error('Pgt file: [{}] is not existed'.format(cls.pgt_file))
             return None
         pgt = None
         if lock.acquire(1):
-            with open(pgt_file, 'rb') as f:
-                row = 0
-                while row < read_rows:
-                    row += 1
-                    line = f.readline()
-                    if not line:
-                        break
-                    line = str(line, encoding='utf-8')
-                    kv = line.split(' = ')
-                    if pgtiou == kv[0].strip():
-                        pgt = kv[1].rstrip('\n').strip()
+            with open(cls.pgt_file, 'rb') as f:
+                line = f.readline()
+                line = str(line, encoding='utf-8')
+                kv = line.split(' = ')
+                if pgtiou == kv[0].strip():
+                    pgt = kv[1].rstrip('\n').strip()
         lock.release()
         return pgt
 
     @classmethod
     def add_pgt_to_file(cls, pgtiou, pgt):
         if lock.acquire(1):
-            if not os.path.exists(pgt_file):
-                os.mknod(pgt_file)
-            with open(pgt_file, 'rb+') as f:
-                lines = f.readlines(read_rows)
+            if not os.path.exists(cls.pgt_file):
+                os.mknod(cls.pgt_file)
+            with open(cls.pgt_file, 'rb+') as f:
+                lines = f.readlines(cls.read_max_bytes)
                 new_line = '{} = {}\n'.format(pgtiou, pgt)
                 lines.insert(0, new_line.encode('utf-8'))
 
