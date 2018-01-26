2300caea23b4feff6ca2f3e9479f3b36a578eab3
rockGithub <rock20110414@gmail.com>
2018-01-26 00:02:19 -0800

    WARP-20858: intl of users page

--- superset/assets/javascripts/user.js
+++ superset/assets/javascripts/user.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
+import intl from "react-intl-universal";
 
 const jQuery = window.jQuery = require('jquery'); // eslint-disable-line
+import { renderLoadingModal, loadIntlResources } from '../utils/utils';
 require('bootstrap');
-
 
 import fetch from 'isomorphic-fetch';
 
@@ -14,17 +15,21 @@ class App extends React.Component {
     state = {
         username: '',
         login_count: '',
-        last_login: ''
+        last_login: '',
+        initDone: false
     }
 
     constructor(props) {
         super(props);
-
     }
 
     componentDidMount() {
         const me = this;
         const url = location.origin + '/present_user/show/';
+
+        loadIntlResources(_ => this.setState({
+            initDone: true
+        }), 'profile');
 
         return fetch(url, {
             credentials: 'include',
@@ -61,42 +66,41 @@ class App extends React.Component {
                         <h4 className="panel-title" style={{
                 marginTop: '50px'
             }}>
-                            用户信息<span className="caret"></span>
+                            {
+            intl.get('INFORMATION')
+            }<span className="caret"></span>
                         </h4>
                     </div>
                     
                     <div id="1_href" className="panel-collapse collapse in">
                         <div className="panel-body">
                             <div className="table-responsive">
-    <table
-            className="table table-bordered"
+                                <table className="table table-bordered"
             style={{
                 margin: '20px auto',
                 fontSize: '12px'
-            }}
-
-            >
+            }}>
                                     <tbody>
                                         <tr>
-                                        <th className="col-lg-2 col-md-2 col-sm-2">用户名</th>
-                                        <td><span style={{
+                                            <th className="col-lg-2 col-md-2 col-sm-2">{intl.get('USER_NAME')}</th>
+                                            <td><span style={{
                 whiteSpace: 'pre-line'
             }}>{username}</span>
-                </td>
+                                            </td>
                                         </tr>
                                         <tr>
-                                        <th className="col-lg-2 col-md-2 col-sm-2">登录次数</th>
-                                        <td><span style={{
+                                            <th className="col-lg-2 col-md-2 col-sm-2">{intl.get('LOGIN_TIMES')}</th>
+                                            <td><span style={{
                 whiteSpace: 'pre-line'
             }}>{login_count}</span>
-                </td>
+                                            </td>
                                         </tr>
-                                                <tr>
-                                        <th className="col-lg-2 col-md-2 col-sm-2">上次登陆时间</th>
-                                        <td><span style={{
+                                        <tr>
+                                            <th className="col-lg-2 col-md-2 col-sm-2">{intl.get('LAST_TIME')}</th>
+                                            <td><span style={{
                 whiteSpace: 'pre-line'
             }}>{last_login}</span>
-                </td>
+                                            </td>
                                         </tr>
                                     </tbody>
                                 </table>
@@ -107,7 +111,7 @@ class App extends React.Component {
 
                 <div className="panel-group" id="2">
                     <div className="well well-sm">
-                        <a href="/back" className="btn btn-sm btn-primary" data-toggle="tooltip" rel="tooltip" title="" data-original-title="后退">
+                        <a href="/back" className="btn btn-sm btn-primary" data-toggle="tooltip" rel="tooltip" title="" data-original-title="{intl.get('GO_BACK')}">
 
                             <i className="fa fa-arrow-left"></i>
                         </a>
