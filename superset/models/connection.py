47ade0aeb7180182c498841bd1a1347a21fd156b
jiajie <jiajie.zhang@transwarp.io>
2018-04-16 15:05:52 +0800

    WARP-23142 use token instead of ticket to get keytab

--- superset/models/connection.py
+++ superset/models/connection.py
@@ -257,13 +257,13 @@ class Database(Model, AuditMixinNullable, ImportMixin):
 
     @classmethod
     def append_args(cls, connect_args):
-        def get_keytab(username, passwd):
+        def get_keytab(username, passwd, token):
             dir = config.get('GLOBAL_FOLDER', '/tmp/pilot')
             if not os.path.exists(dir):
                 os.makedirs(dir)
             path = os.path.join(dir, '{}.keytab'.format(username))
             if conf.get('CAS_AUTH'):
-                download_keytab(username, path)
+                download_keytab(username, path, token=token)
             elif conf.get(GUARDIAN_AUTH):
                 from superset.guardian import guardian_client as client
                 client.login(username, passwd)
@@ -275,15 +275,17 @@ class Database(Model, AuditMixinNullable, ImportMixin):
         def get_ticket():
             raise NotImplementedError
 
+        token = TokenCache.get(g.user.username)
+
         if connect_args.get('mech', '').lower() == 'kerberos':
-            connect_args['keytab'] = get_keytab(g.user.username, g.user.password2)
+            connect_args['keytab'] = get_keytab(g.user.username, g.user.password2, token)
         elif connect_args.get('mech', '').lower() == 'token':
             keys = [k.lower() for k in connect_args.keys()]
             if 'guardiantoken' not in keys:
                 if not config['CAS_AUTH']:
                     raise PropertyException(DISABLE_CAS)
                 else:
-                    connect_args['guardianToken'] = TokenCache.get(g.user.username)
+                    connect_args['guardianToken'] = token
         elif connect_args.get('mech', '').lower() == 'ticket':
             connect_args['casTicket'] = get_ticket()
         return connect_args
